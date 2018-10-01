2d6c058f47a87bd20de2d726bfef077500686532
Isaiah-Turner <isaiahturner42@gmail.com>
2018-10-01 14:51:18 -0400

    Standardized traffic files

--- SUMO_files/setTrafficTimings.py
+++ SUMO_files/setTrafficTimings.py
@@ -2,9 +2,21 @@
 Set traffic light timings by reading in user input. Take in network file and options
 """
 
-import argparse
+import argparse 
+import xml.etree.cElementTree as ET
+
 
 parser = argparse.ArgumentParser(description="Create traffic light timings")
-parser.add_argument("-n",help="input the filename of the network")
+parser.add_argument("-n",help="input the filename of the network or add file with traffic")
 args = parser.parse_args()
-print(args)
+
+tree = ET.parse(args.n)
+root = tree.getroot()
+print(root)
+print(root[0])
+print(root[0][0])
+for light in root:
+    for phase in light:
+        print("Phase is ", phase.attrib)
+#TODO: set phase duration through command line (show each one and let user modify)
+print(args.n)
