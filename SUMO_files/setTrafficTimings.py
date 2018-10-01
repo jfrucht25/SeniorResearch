fa5758ce0c0ccddc8e1d76f3985ab8d39aa766d4
Isaiah-Turner <isaiahturner42@gmail.com>
2018-10-01 15:18:34 -0400

    Completed traffic light timing code

--- SUMO_files/setTrafficTimings.py
+++ SUMO_files/setTrafficTimings.py
@@ -2,7 +2,8 @@
 Set traffic light timings by reading in user input. Take in network file and options
 """
 
-import argparse 
+import argparse
+import msvcrt
 import xml.etree.cElementTree as ET
 
 
@@ -17,6 +18,10 @@ print(root[0])
 print(root[0][0])
 for light in root:
     for phase in light:
+        print(phase.attrib['duration'])
         print("Phase is ", phase.attrib)
+        print("Change timings to:", end="",flush=True)
+        new_timing = input("")
+        phase.attrib['duration'] = new_timing
+tree.write(args.n)
 #TODO: set phase duration through command line (show each one and let user modify)
-print(args.n)
