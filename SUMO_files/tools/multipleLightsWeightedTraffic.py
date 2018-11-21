ea14bb10f2f0b96a19059da50aa1148fc77c1101
Isaiah-Turner <isaiahturner42@gmail.com>
2018-11-21 14:40:37 -0500

    Added lane connections

--- SUMO_files/tools/multipleLightsWeightedTraffic.py
+++ SUMO_files/tools/multipleLightsWeightedTraffic.py
@@ -36,11 +36,11 @@ def calculate_links():
         print("ALL LINKS: ", traci.trafficlight.getControlledLinks(l))
         print("ALL LANES: ", traci.trafficlight.getControlledLanes(l))
         print("-------------------------------------------------------------")
-        for index, lane in enumerate(traci.trafficlight.getControlledLanes(l)):
-            if lane in temp_dict:
-                temp_dict[lane].append(index)
+        for index, controlled_lane in enumerate(traci.trafficlight.getControlledLanes(l)):
+            if controlled_lane in temp_dict:
+                temp_dict[controlled_lane].append(index)
             else:
-                temp_dict[lane] = [index]
+                temp_dict[controlled_lane] = [index]
         if temp_dict:
             to_ret[l] = temp_dict
     return to_ret
@@ -48,13 +48,25 @@ def calculate_links():
 
 def create_subscribers():
     for light in traci.trafficlight.getIDList():
-        for lane in traci.trafficlight.getControlledLanes(light):
-            traci.lane.subscribe(lane, [traci.constants.LAST_STEP_VEHICLE_NUMBER])
+        for controlled_lane in traci.trafficlight.getControlledLanes(light):
+            traci.lane.subscribe(controlled_lane, [traci.constants.LAST_STEP_VEHICLE_NUMBER])
+
+
+def get_link_to_junction(lane_id):
+    to_add = []
+    connections = lane_connections[lane_id]
+    for c in connections:
+        if c in all_controlled_lanes:
+            to_add.append(c)
+        else:
+            to_add.append(get_link_to_junction(c))
+    return to_add
 
 
 default_max_wait = 30
 default_min_wait = 5
 default_weight = 10
+default_incoming_weight = 1
 parser = argparse.ArgumentParser(description="Create traffic light timings based on road lengths")
 parser.add_argument("-s", "--sumocfg", help="input the filename of the SUMO config file")
 parser.add_argument("--gui", default=False, action="store_true", help="Optional: True by default, use for GUI")
@@ -66,7 +78,9 @@ parser.add_argument("--no-red-lane-check", default=False, action="store_true", h
                                                        " program does not look at red lanes to determine switch times")
 parser.add_argument("-w", "--weight", type=int, default=default_weight, help="Optional: %d by default, each car in  a lane represents "
                                                        "this many seconds to the light." % default_weight)
-parser.add_argument("--verbose", action="store_true", default=False, help="Use for more print statements")
+parser.add_argument("--incoming-weight", type=int, default=default_incoming_weight, help="Optional: %d by default, "
+                         "each car coming from another light represents this many senconds." % default_incoming_weight)
+parser.add_argument("--verbose", action="store_true", default=False, help="Use formore print statements")
 
 
 args = parser.parse_args()
@@ -84,16 +98,36 @@ lane_indices = calculate_links()
 create_subscribers()
 
 current_wait = {light_id: 0 for light_id in traci.trafficlight.getIDList()}
+all_controlled_lanes = [lane_id for light_id in traci.trafficlight.getIDList()
+                        for lane_id in traci.trafficlight.getControlledLanes(light_id)]
+lane_connections = {}
+for lane in traci.lane.getIDList():
+    links = traci.lane.getLinks(lane)
+    if links:
+        lane_connections[lane] = [i[0] for i in links]
+print(lane_connections)
+print("------------------------------")
+print(all_controlled_lanes)
+junction_connections = {get_link_to_junction(lane_id) for lane_id in all_controlled_lanes}
+
+
+
 
 while traci.simulation.getMinExpectedNumber() > 0:
     traci.simulationStep()
     step += 1
+    incoming_cars = {lane_id: 0 for lane_id in traci.lane.getIDList()}
     for light_id in traci.trafficlight.getIDList():
         num_vehicles = {}
         for lane_id in traci.trafficlight.getControlledLanes(light_id):
             num_vehicles[lane_id] = traci.lane.getSubscriptionResults(lane_id)[traci.constants.LAST_STEP_VEHICLE_NUMBER]
 
         cars_in_green, cars_in_red = calc_cars_in_red_green(num_vehicles, light_id)
+
+        for lane_id in cars_in_green:
+            cars_in_green[lane_id] += args.incoming_weight * incoming_cars[lane_id]
+        for lane_id in cars_in_red:
+            cars_in_red[lane_id] += args.incoming_weight * incoming_cars[lane_id]
 
         current_wait[light_id] += 1
         if current_wait[light_id] > args.min_wait:
@@ -107,12 +141,13 @@ while traci.simulation.getMinExpectedNumber() > 0:
                 reason = "there are more cars are in red lanes than green"
             else:
                 if cars_in_green.values():
-                    new_duration = int(args.weight * (max(cars_in_green.values()) if args.use_lane_max else average(cars_in_green.values())))
+                    new_duration = int(args.weight * (max(cars_in_green.values()) if args.use_lane_max
+                                                      else average(cars_in_green.values())))
                     if args.use_lane_max:
                         reason = "there are %d cars in lane %s" % (
                             max(cars_in_green.values()), max(cars_in_green, key=cars_in_green.get))
                     else:
-                        reason = "there are an average of %.3f cars in all green lanes" % (
+                        reason = "there are an average of %.3f cars across all green lanes" % (
                             average(cars_in_green.values()))
                 else:
                     new_duration = 0
@@ -122,9 +157,26 @@ while traci.simulation.getMinExpectedNumber() > 0:
             new_duration = traci.trafficlight.getPhaseDuration(light_id)
             reason = "the min duration has not been met"
         if args.verbose:
-            print("Cars in green: ", cars_in_green)
-            print("Cars in red: ", cars_in_red)
-            print("New duration set to %d because %s" % (new_duration, reason))
+            print("\n")
+            print("FOR LIGHT %s: " % light_id)
+            print("    Cars in green: ", cars_in_green)
+            print("    Cars in red: ", cars_in_red)
+            to_print = {i: incoming_cars[i] for i in incoming_cars if incoming_cars[i]}
+            print("    Incoming car (only non-zero shown): ", to_print)
+            print("    New duration set to %d because %s" % (new_duration, reason))
+
         traci.trafficlight.setPhaseDuration(light_id, new_duration)
+        '''
+        New stuff. Check the links from the cars in green lanes and send them through the next lanes.
+        Possible improvement: keep going down connections until at one controlled by a traffic light
+        '''
+        new_cars_in_green, new_cars_in_red = calc_cars_in_red_green(num_vehicles, light_id)
+        for lane_id in new_cars_in_green: # get each green lane
+            for connection in lane_connections[lane_id]: # get each lane it is connected to
+                print(connection in all_controlled_lanes)
+                incoming_cars[connection] += new_cars_in_green[lane_id]/len(lane_connections[lane_id])
+                # for each lane, add number of cars in that lane over total links out from that lane
+                # assumes that cars in a lane equally split between possible lane destinations
+
     print("-------STEP %d OVER-------" % step)
 traci.close()
